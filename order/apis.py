3c628441359e26c35990b2c31f09bab809dafc36
rubayetanjumjoy <rubayetanjumjoy@gmail.com>
2022-10-26 12:15:55 +0600

    cart addedd

--- order/apis.py
+++ order/apis.py
@@ -8,11 +8,24 @@ class cart(APIView):
 
     def get(self,request):
         uuid=User.objects.get(auth_token='24a59d0720b85716f84f664ac5580f7754b03a96')
-        qs=Cart.objects.select_related('user').filter(pk=uuid.id)
-        print(qs)
+        qs=Cart.objects.filter(user__pk=uuid.id)
+        serializer = CartSerializer(qs, many=True)
+        list=[]
+        for obj in qs:
+            dict={}
+            dict["id"]=obj.id
+            dict["quantity"] = obj.quantity
+            dict["user_id"] = obj.user.id
+            dict["product_price"] = obj.item.price
+            dict["product_name"]=obj.item.title
+            dict["total_price"]=obj.quantity*obj.item.price
+
+            list.append(dict)
 
 
 
-        serializer=CartSerializer(qs,many=True)
 
-        return Response(serializer.data)
+
+
+
+        return Response(list)
