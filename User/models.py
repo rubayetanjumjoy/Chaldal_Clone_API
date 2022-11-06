7b61f1a388f19813337e4a494d2771786e8bf8dd
rubayetanjumjoy <rubayetanjumjoy@gmail.com>
2022-11-06 20:00:27 +0600

    floor no field changed

--- User/models.py
+++ User/models.py
@@ -62,7 +62,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 class Address(models.Model):
     address=models.ForeignKey(User,on_delete=models.CASCADE)
     street_address=models.CharField(max_length=200,null=True,blank=True)
-    floor_no=models.IntegerField(null=True,blank=True)
+    floor_no=models.CharField(max_length=200,null=True,blank=True)
     apartment_no=models.CharField(max_length=200,null=True,blank=True)
     def __str__(self):
         return str(self.street_address)
